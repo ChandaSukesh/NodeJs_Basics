0d9166d7740b29f30281430d9ff3510e37cadc60
Chanda Sukesh <chandasukesh@gmail.com>
2024-03-12 00:14:04 +0530

    changed file name

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const number = 9;
+
+if (number < 10) {
+  console.log("small number");
+} else {
+  console.log("Big number");
+}
+
+console.log(`Hey this is my first node program!!`);
--- app.js
+++
@@ -1,9 +0,0 @@
-const number = 9;
-
-if (number < 10) {
-  console.log("small number");
-} else {
-  console.log("Big number");
-}
-
-console.log(`Hey this is my first node program!!`);
