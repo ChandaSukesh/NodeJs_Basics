346f033fc45f5063197904733b1e926345eec459
Chanda Sukesh <chandasukesh@gmail.com>
2024-03-12 00:53:14 +0530

    module example

---
+++ 3-modules-implementation.js
@@ -0,0 +1,6 @@
+const name = require("./4-names");
+const sayHi = require("./5-utils");
+
+sayHi("cupid");
+sayHi(name.sukesh);
+sayHi(name.chanda);
---
+++ 4-names.js
@@ -0,0 +1,9 @@
+// local used in this file itself
+const secretKey = "This is secret key";
+
+// used in different fies
+const sukesh = "sukesh";
+const chanda = "chanda";
+
+// export default
+module.exports = { sukesh, chanda };
