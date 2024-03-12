b7d2c5b4ef5ad2e0d88ecfd42045b263664de63d
Chanda Sukesh <chandasukesh@gmail.com>
2024-03-13 00:07:19 +0530

    uncommented the code

--- 3-modules-implementation.js
+++ 3-modules-implementation.js
@@ -3,8 +3,8 @@ const sayHi = require("./5-utils");
 const data = require("./6-alternative-syntax");
 // console.log("suk1", data);
 
-// sayHi("cupid");
-// sayHi(name.sukesh);
-// sayHi(name.chanda);
+sayHi("cupid");
+sayHi(name.sukesh);
+sayHi(name.chanda);
 
 require("./7-mixed-grenade");
