7badb5d13870f37a8e48c1daca5087309491234c
Chanda Sukesh <chandasukesh@gmail.com>
2024-03-12 23:58:21 +0530

    invoking a module without export

--- 3-modules-implementation.js
+++ 3-modules-implementation.js
@@ -1,7 +1,10 @@
 const name = require("./4-names");
 const sayHi = require("./5-utils");
 const data = require("./6-alternative-syntax");
-console.log("suk1", data);
-sayHi("cupid");
-sayHi(name.sukesh);
-sayHi(name.chanda);
+// console.log("suk1", data);
+
+// sayHi("cupid");
+// sayHi(name.sukesh);
+// sayHi(name.chanda);
+
+require("./7-mixed-grenade");
---
+++ 7-mixed-grenade.js
@@ -0,0 +1,8 @@
+const num1 = 10;
+const num2 = 20;
+
+function sumTwoNumber() {
+  console.log(num1 + num2);
+}
+
+sumTwoNumber();
