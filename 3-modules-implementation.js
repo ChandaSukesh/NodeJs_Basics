9b2e25f3cae5213a66bc7892797e4dcbbe82308c
Chanda Sukesh <chandasukesh@gmail.com>
2024-03-12 23:47:19 +0530

    alternative syntax for module export

--- 3-modules-implementation.js
+++ 3-modules-implementation.js
@@ -1,6 +1,7 @@
 const name = require("./4-names");
 const sayHi = require("./5-utils");
-
+const data = require("./6-alternative-syntax");
+console.log("suk1", data);
 sayHi("cupid");
 sayHi(name.sukesh);
 sayHi(name.chanda);
