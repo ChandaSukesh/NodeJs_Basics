96cab799ffed3d7d80eb4ae90403d6b2185bb0f3
Chanda Sukesh <chandasukesh@gmail.com>
2024-03-15 00:34:34 +0530

    file async examples

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -5,7 +5,7 @@ const second = readFileSync("./content/second.txt", "utf8");
 console.log(first);
 console.log(second);
 
-writeFileSync("./content/result.txt", `The result is:${first},${second}`);
+writeFileSync("./content/result-async.txt", `The result is:${first},${second}`);
 
 // the below one is used for appending the resultant.
 // writeFileSync("./content/result.txt", `The result is:${first},${second}`, {
