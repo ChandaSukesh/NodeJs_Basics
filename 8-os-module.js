fb0c32662e9cbd8f135c31f8f30b61b0a9cf49f2
Chanda Sukesh <chandasukesh@gmail.com>
2024-03-14 23:48:41 +0530

    path module methods

--- 8-os-module.js
+++ 8-os-module.js
@@ -1,12 +1,12 @@
 const os = require("os");
 
 // info about current user
-// console.log(os.userInfo());
+console.log(os.userInfo());
 
 // method that returns the system uptime in seconds
 // uptime is the metric that measures system reliability and stability.
 // if a system uptime is high, then it indicates that the system is more reliable and stable.
-// console.log(os.uptime());
+console.log(os.uptime());
 
 const currentObj = {
   name: os.type(),
