0ae2ac6e30cacb60e21426c70757926c3a77d6fc
Chanda Sukesh <chandasukesh@gmail.com>
2024-03-14 23:28:59 +0530

    os module basic methods

---
+++ 8-OSModule.js
@@ -0,0 +1,9 @@
+const os = require("os");
+
+// info about current user
+// console.log(os.userInfo());
+
+// method that returns the system uptime in seconds
+// uptime is the metric that measures system reliability and stability.
+// if a system uptime is high, then it indicates that the system is more reliable and stable.
+console.log(os.uptime());
