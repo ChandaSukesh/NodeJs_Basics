5bdad29a7cc0bb1697418acfacaa04b776d67dc9
Chanda Sukesh <chandasukesh@gmail.com>
2024-03-14 23:33:26 +0530

    os module different methods

--- 8-OSModule.js
+++ 8-OSModule.js
@@ -6,4 +6,12 @@ const os = require("os");
 // method that returns the system uptime in seconds
 // uptime is the metric that measures system reliability and stability.
 // if a system uptime is high, then it indicates that the system is more reliable and stable.
-console.log(os.uptime());
+// console.log(os.uptime());
+
+const currentObj = {
+  name: os.type(),
+  release: os.release(),
+  totalMemory: os.totalmem(),
+  freeMemory: os.freemem(),
+};
+console.log(currentObj);
