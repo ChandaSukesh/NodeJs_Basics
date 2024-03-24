10751798908c820d0c286020a998d2169352a152
Chanda Sukesh <chandasukesh@gmail.com>
2024-03-24 21:05:12 +0530

    app js file changes

--- express/2-express-app.js
+++ express/2-express-app.js
@@ -1,14 +1,20 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const path = require("path");
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-});
+console.log("suk1", __dirname);
+
+// setting up static and middlewares
+app.use(express.static(`${__dirname}/express/public`));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(`${__dirname}/express`, "./navbar-app/index.html"));
+// });
 
 app.get("*", (req, res) => {
   res.send("resolve your path");
 });
 
 app.listen(5000, () => {
-  console.log("server is listening");
+  console.log("server is listening...1");
 });
