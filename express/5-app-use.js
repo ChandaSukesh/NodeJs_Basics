321867a15230f0bd2570a948b17620dd51c24333
Chanda Sukesh <chandasukesh@gmail.com>
2024-03-25 14:18:32 +0530

    multiple middlewares example

--- express/5-app-use.js
+++ express/5-app-use.js
@@ -9,6 +9,10 @@ app.use(logger);
 //app.use('/api',logger);
 // the above middleware is applicable for all routes which starts with /api ...
 
+// to use multiple middlewares, use below code..
+//  syntax: app.use([middleware1,middleware2])
+// ex: app.use([logger,authorize])
+
 app.get("/", (req, res) => {
   res.send("My Home page");
 });
